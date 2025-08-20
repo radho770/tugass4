619c77e425b841672499777bfea4969e5254cd00
radhoalfareza989 <radhoalfareza989@gmail.com>
2025-08-20 23:06:41 +0700

    persegi

---
+++ luas_persegi.php
@@ -0,0 +1,12 @@
+<?php
+echo "<h2>Rumus Luas Persegi</h2>";
+echo "Rumus: L = S x S<br>";
+
+// Contoh soal
+$sisi = 8;
+echo "Contoh Soal: Berapakah luas persegi dengan sisi $sisi cm?<br>";
+
+// Hitung
+$luas = $sisi * $sisi;
+echo "Jawaban: $sisi x $sisi = $luas cm²";
+?>
