03b4c26b2f387fc1fbed333695861e14a20dea64
radhoalfareza989 <radhoalfareza989@gmail.com>
2025-08-20 23:04:59 +0700

    lingkaran

---
+++ keliling_lingkaran.php
@@ -0,0 +1,13 @@
+<?php
+echo "<h2>Rumus Keliling Lingkaran</h2>";
+echo "Rumus: K = 2 x π x r<br>";
+
+// Contoh soal
+$jari = 7;
+$pi = 3.14;
+echo "Contoh Soal: Berapakah keliling lingkaran dengan jari-jari $jari cm?<br>";
+
+// Hitung
+$keliling = 2 * $pi * $jari;
+echo "Jawaban: 2 x $pi x $jari = $keliling cm";
+?>
