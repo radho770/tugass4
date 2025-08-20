5ed1eb6e94c5de5775d16354c7210e4639be7844
radhoalfareza989 <radhoalfareza989@gmail.com>
2025-08-20 23:08:33 +0700

    kubus

---
+++ volume_kubus.php
@@ -0,0 +1,12 @@
+<?php
+echo "<h2>Rumus Volume Kubus</h2>";
+echo "Rumus: V = S x S x S<br>";
+
+// Contoh soal
+$sisi = 5;
+echo "Contoh Soal: Berapakah volume kubus dengan sisi $sisi cm?<br>";
+
+// Hitung
+$volume = $sisi * $sisi * $sisi;
+echo "Jawaban: $sisi x $sisi x $sisi = $volume cm³";
+?>
